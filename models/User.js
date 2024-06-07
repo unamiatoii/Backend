46e3c28dd86d2d051e6751ee2c86c592719f61ab
unamiatoii <pierrejaphetkonan@gmail.com>
2024-06-07 11:54:54 +0000

    Modification schema User

--- models/User.js
+++ models/User.js
@@ -6,6 +6,17 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  phone: { type: String, required: true },
+  role: { type: String, enum: ['customer', 'admin'], default: 'customer' },
+}, {
+  timestamps: true,
 });
 
 // Hash the password before saving the user
